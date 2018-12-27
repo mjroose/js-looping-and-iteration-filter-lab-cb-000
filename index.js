6c8d388c6bea2d4f1b16ed70a1527f44c0bef505
Michael Roose <mjroose@gmail.com>
2018-12-27 20:33:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file
 const findMatching = (drivers, query) => {
   const lowerCaseQuery = query.toLowerCase();
+  console.log(lowerCaseQuery);
   drivers.filter( (driver) => { driver.toLowerCase() === lowerCaseQuery } );
 };
